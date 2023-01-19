b9c01b4540a24ec6e34d8759e4ddb5c947ab82c0
Mohamad-Kamar <mohamad.kamar.dev@gmail.com>
2023-01-19 21:30:41 +0200

    feat: set up queue view with mock data

--- src/app/queue-view/queue-view.component.ts
+++ src/app/queue-view/queue-view.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { Queue } from '../Types/Queue';
 
+
+const QUEUES: Queue[] = [
+	{
+    queueType: "fanout",
+    queueKey: 'TQ1',
+	},
+	{
+    queueType: "direct",
+    queueKey: 'TQ2',
+	},
+  {
+    queueType: "roundrobin",
+    queueKey: 'TQ3',
+	},
+];
 @Component({
   selector: 'app-queue-view',
   templateUrl: './queue-view.component.html',
-  styleUrls: ['./queue-view.component.css']
 })
 export class QueueViewComponent {
-
+	queues$: Observable<Queue[]> = of(QUEUES);
 }

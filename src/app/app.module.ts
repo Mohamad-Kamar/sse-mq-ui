53a9767568b875717ad26cca637ab0e96e740e56
Mohamad-Kamar <mohamad.kamar.dev@gmail.com>
2023-01-19 20:20:50 +0200

    feat: add consumer view component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { MessageViewComponent } from './message-view/message-view.component';
 import { QueueViewComponent } from './queue-view/queue-view.component';
+import { ConsumerViewComponent } from './consumer-view/consumer-view.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MessageViewComponent,
     QueueViewComponent,
+    ConsumerViewComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/consumer-view/consumer-view.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-consumer-view',
+  templateUrl: './consumer-view.component.html',
+  styleUrls: ['./consumer-view.component.css']
+})
+export class ConsumerViewComponent {
+
+}

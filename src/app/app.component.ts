413637c567a30ea3577e7d0f8f7de38aaa1143a0
Mohamad-Kamar <mohamad.kamar.dev@gmail.com>
2023-01-19 20:35:17 +0200

    feat: add navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'sse-mq-ui';
+  queue = 'Queues';
+  message = 'Messages';
+  consumer = 'Consumers';
+	active = this.queue;
 }

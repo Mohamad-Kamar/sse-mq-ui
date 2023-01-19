607c95716b646325d3ad65388e067d9b6ea97794
Mohamad-Kamar <mohamad.kamar.dev@gmail.com>
2023-01-20 01:42:47 +0200

    feat: make use of config service in consumer service

--- src/app/consumer-view/consumer-view.service.ts
+++ src/app/consumer-view/consumer-view.service.ts
@@ -2,24 +2,32 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Consumer } from '../Types/Consumer';
 import { map, Observable } from 'rxjs';
+import { ConfigService } from '../config.service';
 
 export type ConsumerCollection = {
   [consumerID: string]: Consumer;
 };
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ConsumerViewService {
-  baseURL = 'http://localhost:3491/consumer'
+  url: string;
   consumers$: Observable<Consumer[]>;
-  constructor(private http: HttpClient){
-    this.consumers$ = this.http.get<ConsumerCollection>(this.baseURL)
-    .pipe(
-      map((consumerCollection: ConsumerCollection) => Object.values(consumerCollection))
-    );
+  constructor(
+    private http: HttpClient,
+    private readonly configService: ConfigService
+  ) {
+    this.url = this.configService.getBaseUrl() + '/consumer';
+    this.consumers$ = this.http
+      .get<ConsumerCollection>(this.url)
+      .pipe(
+        map((consumerCollection: ConsumerCollection) =>
+          Object.values(consumerCollection)
+        )
+      );
   }
-  getConsumers(){
-    return this.consumers$
+  getConsumers() {
+    return this.consumers$;
   }
 }

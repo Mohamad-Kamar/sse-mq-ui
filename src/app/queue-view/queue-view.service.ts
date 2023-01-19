2e9af5112109da1016f0ecf1eef17a4fcba385d5
Mohamad-Kamar <mohamad.kamar.dev@gmail.com>
2023-01-20 01:39:05 +0200

    feat: make use of config service in queue service

--- src/app/queue-view/queue-view.service.ts
+++ src/app/queue-view/queue-view.service.ts
@@ -2,22 +2,30 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Queue } from '../Types/Queue';
 import { map, Observable } from 'rxjs';
+import { ConfigService } from '../config.service';
 
-type QueueCollection = {[queueKey: string]: Queue};
+type QueueCollection = { [queueKey: string]: Queue };
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class QueueViewService {
-  baseURL = 'http://localhost:3491/queue'
+  url: string;
   queues$: Observable<Queue[]>;
-  constructor(private http: HttpClient){
-    this.queues$ = this.http.get<QueueCollection>(this.baseURL)
-    .pipe(
-      map((queuesCollection: QueueCollection) => Object.values(queuesCollection))
-    );
+  constructor(
+    private http: HttpClient,
+    private readonly configService: ConfigService
+  ) {
+    this.url = this.configService.getBaseUrl() + '/queue';
+    this.queues$ = this.http
+      .get<QueueCollection>(this.url)
+      .pipe(
+        map((queuesCollection: QueueCollection) =>
+          Object.values(queuesCollection)
+        )
+      );
   }
-  getQueues(){
-    return this.queues$
+  getQueues() {
+    return this.queues$;
   }
 }

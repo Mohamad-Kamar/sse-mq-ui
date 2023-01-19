4900be1e8bdf4c4e875ec433c9d1166af9f58c18
Mohamad-Kamar <mohamad.kamar.dev@gmail.com>
2023-01-19 21:17:33 +0200

    feat: add messages page with fake data

--- src/app/message-view/message-view.component.ts
+++ src/app/message-view/message-view.component.ts
@@ -1,10 +1,45 @@
 import { Component } from '@angular/core';
+import { Observable, of } from 'rxjs';
+
+import { Message } from '../Types/Message';
+import { MessageViewService } from './message-view.service';
+
+const MESSAGES: Message[] = [
+	{
+    messageID: "1",
+    messageContent: "Message 1",
+    durable: true,
+    consumerID: "TC1",
+	},
+	{
+    messageID: "2",
+    messageContent: "Message 2",
+    durable: true,
+    consumerID: "TC1",
+	},
+	{
+    messageID: "3",
+    messageContent: "Message 3",
+    durable: true,
+    consumerID: "TC2",
+	},
+	{
+    messageID: "4",
+    messageContent: "Message 4",
+    durable: true,
+    consumerID: "TC2",
+	},
+];
+
 
 @Component({
   selector: 'app-message-view',
   templateUrl: './message-view.component.html',
-  styleUrls: ['./message-view.component.css']
+  styleUrls: ['./message-view.component.css'],
+  providers: [MessageViewService],
 })
+
 export class MessageViewComponent {
+	messages$: Observable<Message[]> = of(MESSAGES);
 
 }

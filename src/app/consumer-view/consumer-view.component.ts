ccba992fe67d35498c90720083337c88840c9163
Mohamad-Kamar <mohamad.kamar.dev@gmail.com>
2023-01-19 21:25:27 +0200

    feat: improve consumer view with mock data

--- src/app/consumer-view/consumer-view.component.ts
+++ src/app/consumer-view/consumer-view.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { Consumer } from '../Types/Consumer';
+
+
+const CONSUMERS: Consumer[] = [
+	{
+    consumerID: "TC1",
+    queueKey: 'TQ1',
+	},
+	{
+    consumerID: "TC2",
+    queueKey: 'TQ1',
+	},
+  {
+    consumerID: "TC3",
+    queueKey: 'TQ2',
+	},
+];
 
 @Component({
   selector: 'app-consumer-view',
   templateUrl: './consumer-view.component.html',
-  styleUrls: ['./consumer-view.component.css']
 })
 export class ConsumerViewComponent {
-
+	consumers$: Observable<Consumer[]> = of(CONSUMERS);
 }

56418c7906f73dd6b7df8eb5966d9ecab5b9ec7a
Mohamad-Kamar <mohamad.kamar.dev@gmail.com>
2023-01-19 20:20:02 +0200

    feat: add message-view component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { MessageViewComponent } from './message-view/message-view.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    MessageViewComponent,
   ],
   imports: [
     BrowserModule,
